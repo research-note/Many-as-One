e06c86fb06dff217cd22430c75d2a8fe1ef41f55
paranlee <p4ranlee@gmail.com>
2021-10-27 13:33:10 +0900

    add copyright comment

--- collage/libcollage/tables/TableCursor.hpp
+++ collage/libcollage/tables/TableCursor.hpp
@@ -1,5 +1,20 @@
-#ifndef TABLE_CURSOR_H
-#define TABLE_CURSOR_H
+/* Copyright 2021 The Many as One Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef TABLE_CURSOR_HPP
+#define TABLE_CURSOR_HPP
 
 #include <cstdint>
 #include <memory>
@@ -78,4 +93,4 @@ namespace db {
 
 };
 
-#endif // TABLE_CURSOR_H
+#endif // TABLE_CURSOR_HPP
--- nn/simplenet/main/Simplenet.hpp
+++ nn/simplenet/main/Simplenet.hpp
@@ -1,12 +1,20 @@
-/*
- * Build obj linking with cc_library Simplenet Class.
- *
- * Copyright Bazel organization
- *
- */
+/* Copyright 2021 The Many as One Authors. All Rights Reserved.
 
-#ifndef CH04_SIMPLENET_MAIN_SIMPLENET_H_
-#define CH04_SIMPLENET_MAIN_SIMPLENET_H_
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef NN_SIMPLENET_MAIN_SIMPLENET_H_
+#define NN_SIMPLENET_MAIN_SIMPLENET_H_
 
 #include <vector>
 #include <optional>
@@ -50,4 +58,4 @@ private:
 
 #include "Simplenet.cc"
 
-#endif  // CH04_SIMPLENET_MAIN_SIMPLENET_H_
+#endif  // NN_SIMPLENET_MAIN_SIMPLENET_H_
