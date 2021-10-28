b00bf874dccec0d44ae0d8a821405d5aa8033d28
youga114 <youga114@gmail.com>
2021-10-28 22:16:04 +0900

    [Parser] Add copyright

--- nn/parser/main/CParser.hpp
+++ nn/parser/main/CParser.hpp
@@ -1,64 +1,17 @@
-// #if !defined(UPNG_H)
-// #define UPNG_H
+/* Copyright 2021 The Many as One Authors. All Rights Reserved.
 
-// typedef enum upng_error {
-// 	UPNG_EOK			= 0, /* success (no error) */
-// 	UPNG_ENOMEM			= 1, /* memory allocation failed */
-// 	UPNG_ENOTFOUND		= 2, /* resource not found (file missing) */
-// 	UPNG_ENOTPNG		= 3, /* image data does not have a PNG header */
-// 	UPNG_EMALFORMED		= 4, /* image data is not a valid PNG image */
-// 	UPNG_EUNSUPPORTED	= 5, /* critical PNG chunk type is not supported */
-// 	UPNG_EUNINTERLACED	= 6, /* image interlacing is not supported */
-// 	UPNG_EUNFORMAT		= 7, /* image color format is not supported */
-// 	UPNG_EPARAM			= 8  /* invalid parameter to method call */
-// } upng_error;
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-// typedef enum upng_format {
-// 	UPNG_BADFORMAT,
-// 	UPNG_RGB8,
-// 	UPNG_RGB16,
-// 	UPNG_RGBA8,
-// 	UPNG_RGBA16,
-// 	UPNG_LUMINANCE1,
-// 	UPNG_LUMINANCE2,
-// 	UPNG_LUMINANCE4,
-// 	UPNG_LUMINANCE8,
-// 	UPNG_LUMINANCE_ALPHA1,
-// 	UPNG_LUMINANCE_ALPHA2,
-// 	UPNG_LUMINANCE_ALPHA4,
-// 	UPNG_LUMINANCE_ALPHA8,
-// 	UPNG_INDEX1,
-// 	UPNG_INDEX2,
-// 	UPNG_INDEX4,
-// 	UPNG_INDEX8
-// } upng_format;
+    http://www.apache.org/licenses/LICENSE-2.0
 
-// typedef struct upng_t upng_t;
-
-// upng_t*		upng_new_from_bytes	(const unsigned char* buffer, unsigned long size);
-// upng_t*		upng_new_from_file	(const char* path);
-// void		upng_free			(upng_t* upng);
-
-// upng_error	upng_header			(upng_t* upng);
-// upng_error	upng_decode			(upng_t* upng);
-
-// upng_error	upng_get_error		(const upng_t* upng);
-// unsigned	upng_get_error_line	(const upng_t* upng);
-
-// unsigned	upng_get_width		(const upng_t* upng);
-// unsigned	upng_get_height		(const upng_t* upng);
-// unsigned	upng_get_bpp		(const upng_t* upng);
-// unsigned	upng_get_bitdepth	(const upng_t* upng);
-// unsigned	upng_get_components	(const upng_t* upng);
-// unsigned	upng_get_pixelsize	(const upng_t* upng);
-// upng_format	upng_get_format		(const upng_t* upng);
-
-// const unsigned char*	upng_get_buffer		(const upng_t* upng);
-// unsigned				upng_get_size		(const upng_t* upng);
-// const unsigned char*	upng_get_palette	(const upng_t* upng);
-
-// #endif /*defined(UPNG_H)*/
-
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
 
 #ifndef _NN_CLASS_PARSER_HPP_
 #define _NN_CLASS_PARSER_HPP_
--- nn/parser/main/example.cc
+++ nn/parser/main/example.cc
@@ -22,21 +22,21 @@ int main(void)
 {
 	CParser parser;
     
-    // // mnist
-	// parser.loadMnist();
+    // mnist
+	parser.loadMnist();
 
-    // cout<<fixed;
-    // cout.precision(1);
-    // for (int i=0; i<28; ++i) {
-    //     for (int j=0; j<28; ++j) {
-    //         cout << test_image[0][(i * 28) + j] << " ";
-    //     }
-    //     cout << endl;
-    // }
+    cout<<fixed;
+    cout.precision(1);
+    for (int i=0; i<28; ++i) {
+        for (int j=0; j<28; ++j) {
+            cout << test_image[0][(i * 28) + j] << " ";
+        }
+        cout << endl;
+    }
 
 
-    // png
-    parser.loadPng("./png_data/fruits.png");
+    // // png
+    // parser.loadPng("./png_data/fruits.png");
 
 	return 0;
 }
